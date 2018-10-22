a5744c1d5fa12a7726ca0bb6cd5fbc2cb423f525
user654name <ztqztq@qq.com>
2018-10-22 14:33:25 +0800

    2018年10月22日 14:33:01 优化

--- BuyWhatModule/src/main/java/com/buywhat/demo/DemoApplication.java
+++ BuyWhatModule/src/main/java/com/buywhat/demo/DemoApplication.java
@@ -2,8 +2,6 @@ package com.buywhat.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
